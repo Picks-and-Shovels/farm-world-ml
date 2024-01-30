decfc14ba1f9acab5999fea5631b406543618aee
js0807 <js0807@hanyang.ac.kr>
2024-01-31 00:21:11 +0900

    fix : dataset error - contain similarity correctly

--- recommendation/recommend.py
+++ recommendation/recommend.py
@@ -9,20 +9,17 @@ import seaborn as sns
 
 from sklearn.metrics.pairwise import cosine_similarity
 
-user_data = pd.read_csv("data/user.csv")
-farm_data = pd.read_csv("data/farm.csv")
-tag_data = pd.read_csv("data/tag.csv")
-board_data = pd.read_csv("data/board.csv")
+board_data = pd.read_csv("data/data.csv")
 
-df_user_board = board_data.pivot(
-    index='writerId',
-    columns='title',
-    values='views'
-).fillna(0)
+df_user_board = board_data.pivot_table(
+	values='views',
+	index='userId',
+	columns='title',
+	aggfunc='sum').fillna(0)
 
 df_user_board = df_user_board.transpose()
 
 sim = cosine_similarity(df_user_board, df_user_board)
 sim_df = pd.DataFrame(data=sim, index=df_user_board.index, columns=df_user_board.index)
 
-print(sim_df["girl"].sort_values(ascending=False)[0:3])
+print(sim_df["09DP70A8"].sort_values(ascending=False)[1:10])
